e41ed1bb67ed761a0cc46d07f6b330d81694e298
mann909 <152745629+mann909@users.noreply.github.com>
2024-09-29 15:45:15 +0530

    Delete backend directory

--- backend/admin/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'intrest', 'phoneNumber')
-
-admin.site.register(User, UserAdmin)
-admin.site.register(File)
--- backend/admin/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path("user/signup", UserSignupView.as_view()),
-    path("user/signin", UserSigninView.as_view()),
-    path("user/getUser", GetUserView.as_view()),
-    path("file/postFile", FileView.as_view())
-]
